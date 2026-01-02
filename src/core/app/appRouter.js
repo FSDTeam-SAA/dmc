0fa3a33cde5976649d79439fc256e8c9ba93474d
jarifMohammed <mohammedjarif@protonmail.com>
2026-01-02 06:00:15 +0600

    feat: add content management functionality with CRUD operations

--- src/core/app/appRouter.js
+++ src/core/app/appRouter.js
@@ -4,6 +4,7 @@ import userRoutes from '../../entities/user/user.routes.js';
 import pricingTierRoutes from '../../entities/admin/pricing.routes.js'
 import generateImage from '../../entities/GEMINI/gemini.route.js'
 import orderRoutes from '../../entities/order/order.routes.js'
+import contentRoutes  from '../../entities/admin/content/content.routes.js'
 const router = express.Router();
 
 
@@ -12,6 +13,7 @@ router.use('/v1/user', userRoutes);
 router.use('/v1/pricing',pricingTierRoutes)
 router.use('/v1/ai',generateImage)
 router.use('/v1/order',orderRoutes)
+router.use('/v1/content',contentRoutes)
 
 
 export default router;
